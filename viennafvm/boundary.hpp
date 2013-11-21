45a7ec6a838e915fa0e1df9f207e729530d0b341
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2013-11-21 18:01:05 +0100

    added addto_dirichlet_boundary() and two additional set_initial_value() fucntions
    @addto_dirichlet_boundary(): adds a value to an already assigned value
    identified via the quantity -> if the cell hasn't been already tagged
    as dirichlet, an assignemnt is performed rather than an addition
    
    @set_initial_value(quan, seg, !source!): this allows for a quantity transfer
    via another quantity
    
    @set_initial_value(quan, seg, !functor!): this enables to set the initial
    value via a unary_functor, requiring the cell object and returns a value.
    --> is used in ViennaMini to compute the cell-based builtin_potential based on the
    acceptor and donator quantities --> will be important if we don't have
    an academical step-wise doping

--- viennafvm/boundary.hpp
+++ viennafvm/boundary.hpp
@@ -32,6 +32,7 @@
 namespace viennafvm
 {
 
+  /** @brief Assigns a value to the quantity of all cells of the segment or mesh */
   template <typename QuantityType, typename DomainSegmentType>
   void set_dirichlet_boundary(QuantityType & quan,
                               DomainSegmentType const & seg,
@@ -53,7 +54,41 @@ namespace viennafvm
     }
   }
 
+  /** @brief Adds a value to an already assigned quantity value on all cells of the segment or mesh */
+  template <typename QuantityType, typename DomainSegmentType>
+  void addto_dirichlet_boundary(QuantityType & quan,
+                              DomainSegmentType const & seg,
+                              double value)
+  {
+    typedef typename viennagrid::result_of::cell_tag<DomainSegmentType>::type CellTag;
 
+    typedef typename viennagrid::result_of::element<DomainSegmentType, CellTag>::type               CellType;
+    typedef typename viennagrid::result_of::const_element_range<DomainSegmentType, CellTag>::type  CellContainer;
+    typedef typename viennagrid::result_of::iterator<CellContainer>::type                       CellIterator;
+
+    CellContainer cells(seg);
+    for (CellIterator cit  = cells.begin();
+                      cit != cells.end();
+                    ++cit)
+    {
+      // if this cell has already been assigned a dirichlet value,
+      // add the new value to the old one
+      if(quan.get_boundary_type(*cit) == viennafvm::BOUNDARY_DIRICHLET)
+      {
+        quan.set_boundary_value(*cit, quan.get_boundary_value(*cit) + value);
+      }
+      // if this cell has infact not been assigend a dirichlet value,
+      // simply assign the new value
+      //
+      else
+      {
+        quan.set_boundary_value(*cit, value);
+        quan.set_boundary_type(*cit, viennafvm::BOUNDARY_DIRICHLET);
+      }
+    }
+  }
+
+  /** @brief Set the values of a quantity on all cells with a constant value */
   template <typename QuantityType, typename DomainSegmentType>
   void set_initial_value(QuantityType & quan,
                          DomainSegmentType const & seg,
@@ -74,6 +109,49 @@ namespace viennafvm
     }
   }
 
+  /** @brief Transfer the quantity values from another quantity */
+  template <typename QuantityType, typename DomainSegmentType>
+  void set_initial_value(QuantityType & quan,
+                         DomainSegmentType const & seg,
+                         QuantityType & source)
+  {
+    typedef typename viennagrid::result_of::cell_tag<DomainSegmentType>::type CellTag;
+
+    typedef typename viennagrid::result_of::element<DomainSegmentType, CellTag>::type               CellType;
+    typedef typename viennagrid::result_of::const_element_range<DomainSegmentType, CellTag>::type  CellContainer;
+    typedef typename viennagrid::result_of::iterator<CellContainer>::type                       CellIterator;
+
+    CellContainer cells(seg);
+    for (CellIterator cit  = cells.begin();
+                      cit != cells.end();
+                    ++cit)
+    {
+      quan.set_value(*cit, source.get_value(*cit));
+    }
+  }
+
+  /** @brief Assign the quantity based on an unary functor returning cell-based values */
+  template <typename QuantityType, typename DomainSegmentType, typename FunctorT>
+  void set_initial_value(QuantityType & quan,
+                         DomainSegmentType const & seg,
+                         FunctorT       functor)
+  {
+    typedef typename viennagrid::result_of::cell_tag<DomainSegmentType>::type CellTag;
+
+    typedef typename viennagrid::result_of::element<DomainSegmentType, CellTag>::type               CellType;
+    typedef typename viennagrid::result_of::const_element_range<DomainSegmentType, CellTag>::type  CellContainer;
+    typedef typename viennagrid::result_of::iterator<CellContainer>::type                       CellIterator;
+
+    CellContainer cells(seg);
+    for (CellIterator cit  = cells.begin();
+                      cit != cells.end();
+                    ++cit)
+    {
+      quan.set_value(*cit, functor(*cit));
+    }
+  }
+
+  /** @brief Tag the quantity as a solution quantity */
   template <typename QuantityType, typename DomainSegmentType>
   void set_unknown(QuantityType & quan,
                    DomainSegmentType const & seg,
