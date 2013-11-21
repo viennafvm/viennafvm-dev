2da5aa826b18187b6579a67a758007a5497b1577
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2013-11-21 16:40:42 +0100

    extended problem_description: link_mesh(), has_mesh(), has_quantity()

--- viennafvm/problem_description.hpp
+++ viennafvm/problem_description.hpp
@@ -37,6 +37,11 @@ namespace viennafvm
     quantity_not_found_exception(std::string const & str) : std::runtime_error(str) {}
   };
 
+  /** @brief Exception for the case that the mesh hasn't been set */
+  class mesh_not_found_exception : public std::runtime_error {
+  public:
+    mesh_not_found_exception() : std::runtime_error("No mesh available!") {}
+  };
 
   template<typename MeshT>
   class problem_description
@@ -56,15 +61,18 @@ namespace viennafvm
       typedef MeshT        MeshType;
       typedef MeshType     mesh_type;
 
-      problem_description() : mesh_(NULL) {}  // set mesh via operator= or copy-CTOR
+      problem_description() : mesh_(NULL) {}
 
-      problem_description(MeshT const & mesh) : mesh_(&mesh) {}
+      problem_description(MeshT const & mesh) : mesh_(&mesh) {}  // set mesh via operator= or copy-CTOR
+
+      void link_mesh(MeshT const & mesh) { mesh_ = &mesh; }       // set mesh via memberfunction
 
       quantity_container_type const & quantities() const { return quantities_; }
       quantity_container_type       & quantities()       { return quantities_; }
 
       QuantityType & add_quantity(std::string name, numeric_type default_value = numeric_type())
       {
+        if(!mesh_) throw mesh_not_found_exception();
         quantities_.push_back(QuantityType(quantities_.size(), name, viennagrid::cells(*mesh_).size(), default_value));
         return quantities_.back();
       }
@@ -87,7 +95,40 @@ namespace viennafvm
         throw quantity_not_found_exception(name);
       }
 
+      bool has_quantity(std::string name)
+      {
+        for (std::size_t i=0; i<quantities_.size(); ++i)
+          if (quantities_[i].get_name() == name)
+            return true;
+        return false;
+      }
+
+      bool has_quantity(std::string name) const
+      {
+        for (std::size_t i=0; i<quantities_.size(); ++i)
+          if (quantities_[i].get_name() == name)
+            return true;
+        return false;
+      }
+
+      bool has_mesh()
+      {
+        if(mesh_) return true;
+        else      return false;
+      }
+
+      bool has_mesh() const
+      {
+        if(mesh_) return true;
+        else      return false;
+      }
+
       MeshType const & mesh() const { return *mesh_; }
+
+      void clear_quantities()
+      {
+        quantities_.clear();
+      }
 
     private:
       MeshType const *         mesh_;
